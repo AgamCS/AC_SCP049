4a9fc605b6c10beca8ba73ffa1bd98a4cbd91bdb
AgamCS <cherraagam@gmail.com>
2022-07-02 00:34:59 -0700

    added serverside verfication

--- AC_SCP-049/player/sh_playerMeta.lua
+++ AC_SCP-049/player/sh_playerMeta.lua
@@ -2,6 +2,7 @@ local _P = FindMetaTable("Player")
 
 
 function _P:addCure(cureType)
+    if CLIENT then return end
     if !cureType then return end
     local cure = AC_SCP49.getCure(cureType)
     if !cure then return end
@@ -12,7 +13,12 @@ function _P:addCure(cureType)
 end
 
 function _P:getCures()
-    return cureType
+    if SERVER then
+        return cureType
+    else
+        net.Start("ac_scp049.requestCureList")
+        net.SendToServer()
+    end
 end
 
 function _P:removeCure(cureName)
@@ -34,7 +40,7 @@ end
 
 function _P:applyCure(cureType, spawnPos)
     if !SERVER then return end
-    if !self.cures[cureType] then return end
+    if !self.cures[cureType] || self.cures[cureType] < 1 then return end
     cureType = AC_SCP49.getCure(cureType) 
     if !cureType then return end
     cureType.effect(self)
@@ -42,6 +48,7 @@ function _P:applyCure(cureType, spawnPos)
     self:Spawn()
     self:SetPos(spawnPos)
     self.setIs0492(true)
+    self.cures[cureType].amount = self.cures[cureType].amount - 1
 end
 
 
@@ -52,21 +59,28 @@ end
 function _P:setIsMixing(cureType, state)
     if (!cureType || !AC_SCP49.getCure(cureType)) && state == true then return end
     state = tobool(state)
-    if state == true && self.cureCount >= AC_SCP49.config.cureLimit then DarkRP.notify(self, 1, 8, AC_SCP49.getLang("cure_limit")) return end
-    self.isPlayerMixing = state
     if CLIENT then
+        self.isPlayerMixing = state
         net.Start("ac_scp049.startMix")
             net.WriteBool(state)
             if cureType then
                 net.WriteString(cureType)
             end
         net.SendToServer()
+        if state == false then return end
+        timer.Create(self:SteamID64() .. " ac_scp049.mixingTimer", AC_SCP49.config.mixTime, 1, function()
+            self:setIsMixing(false)
+        end)
     end
-    if state == false then return end
-    timer.Create(self:SteamID64() .. " ac_scp049.mixingTimer", AC_SCP49.config.mixTime, 1, function()
-        self:setIsMixing(false)
-        self:addCure(cureType)
-    end)
+    if SERVER then
+        if state == true && self.cureCount >= AC_SCP49.config.cureLimit then DarkRP.notify(self, 1, 8, AC_SCP49.getLang("cure_limit")) return end
+        self.isPlayerMixing = state
+        if state == false then return end
+        timer.Create(self:SteamID64() .. " ac_scp049.mixingTimer", AC_SCP49.config.mixTime, 1, function()
+            self:setIsMixing(false)
+            self:addCure(cureType)
+        end)
+    end
     
 end
 
@@ -82,6 +96,7 @@ if SERVER then
         ply:setIsMixing(false)
         ply.cures = {}
         ply.cureCount = 0
+        ply.currentCure = ""
     end)
 
     net.Receive("ac_scp049.startMix", function(len, ply)
@@ -90,15 +105,31 @@ if SERVER then
         ply:setIsMixing(cure, state)
     end)
 
-
+    net.Receive("ac_scp049.requestCureList", function(len, ply)
+        net.Start("ac_scp049.requestCureList")
+            local json = util.TableToJSON(self.cures)
+            json = util.Compress(json)
+            local len = #json
+            net.WriteUInt(len, 8)
+            net.WriteData(json, len)
+        net.Send(ply)
+    end)
 end
 
 if CLIENT then
     hook.Add("InitPostEntity", "AC_SCP049.SetupPlayer", function()
         net.Start("ac_scp049.setupPlayer")
         net.SendToServer()
-        LocalPlayer().cures = {}
-        ply.cureCount
+        ply.cures = {}
+        ply.currentCure = ""
+    end)
+    
+    net.Receive("ac_scp049.requestCureList", function()
+        local len = net.ReadUInt(8)
+        local data = net.ReadData(len)
+        data = util.Decompress(data)
+        data = util.JSONToTable(data)
+        LocalPlayer().cures = data
     end)
 end
 

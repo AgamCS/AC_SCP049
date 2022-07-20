4a62e421e7c8e52a4b3df2165d791f2107800c24
AgamCS <cherraagam@gmail.com>
2022-07-20 15:01:34 -0700

    fix

--- AC_SCP-049/zombies/sh_zombie_manager.lua
+++ AC_SCP-049/zombies/sh_zombie_manager.lua
@@ -4,12 +4,23 @@ AC_SCP49.zombie.list = AC_SCP49.zombie.list or {}
 function AC_SCP49.zombie.Add(entity)
     if !SERVER then return end
     table.insert(self.list, entity)
+    net.Start("ac_scp049.sendZombieAdd")
+        net.WriteEntity(entity)
+    net.Broadcast()
+end
+
+function AC_SCP49.zombie.Remove(entity)
+    table.RemoveByValue(self.list, entity)
 end
 
 if CLIENT then
     local zombieOutline = AC_SCP49.config.zombieOutlineColor
-    hook.Add("PreDrawHalos", "AC_SCP049.drawZombieOutlines", function()
 
+    hook.Add("PreDrawHalos", "AC_SCP049.drawZombieOutlines", function()
+        if table.IsEmpty(AC_SCP49.zombie.list) then return end
+        local ply = LocalPlayer()
+        if !ply.is0492() || !ply.isSCP049() then return end
+        outline.Add(AC_SCP49.zombie.list, zombieOutline, OUTLINE_MODE_BOTH)
     end)
 end
 
